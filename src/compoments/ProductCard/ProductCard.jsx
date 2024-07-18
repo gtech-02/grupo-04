a4cb9b6b331807b3796f06f050b35f0f81f920ef
PedroGui404 <guilhermemorais0003@gmail.com>
2024-07-18 02:11:59 -0300

    Criando Componente Card

---
+++ src/compoments/ProductCard/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ProductCard.css';
+import productImage from "../../../public/produc-image-5.jpeg"
+
+
+const Card = () => {
+    return (
+        <div className="card" style={{ width: '18rem', margin: '20px auto' }}>
+            <img src={productImage} className="card-img-top" alt="Tênis K-Swiss V8" />
+            <div className="card-body">
+                <p className="card-text text-muted">Tênis</p>
+                <h5 className="card-title">K-Swiss V8 - Masculino</h5>
+                <p>
+                    <span className="old-price">$200</span>
+                    <span className="new-price">$100</span>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+
+export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Layout from "../compoments/Layout";
+import Card from "../compoments/ProductCard/ProductCard";
 
 const HomePage = () => {
     return ( 
@@ -7,6 +8,9 @@ const HomePage = () => {
             <h1>Home Page</h1>
             <h2>Bem-vindo a Home page</h2>
         </Layout>
+        <div>
+            <Card />
+        </div>
 
 
         </>
