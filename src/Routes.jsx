590a450dbc24bd8e80d29948702e3ce5b826c49c
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2024-07-19 15:48:06 -0300

    adicionando o arquivo de rotas

---
+++ src/Routes.jsx
@@ -0,0 +1,20 @@
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Route, Routes,  Navigate } from 'react-router-dom';
+import ProductViewPage from './pages/ProductViewPage';
+import ProductListiningPage from './pages/ProductListingPage';
+
+
+const Rotas = () => {
+    return ( 
+        <Router>
+        <Routes>
+            <Route exact path="/home" element={<HomePage />} />
+            <Route path="/products" element={<ProductViewPage/>} />
+            <Route path="/categories" element={<ProductListiningPage />} />
+            <Route path='*' element={<Navigate to='/home' />} />
+        </Routes>
+    </Router>
+     );
+}
+ 
+export default Rotas;
