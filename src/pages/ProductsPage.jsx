df2277e04cfc5a44722e46ac03a54cb00dd9067f
JEFFERSON WILLIAM FRUTUOSO DA CRUZ <JEFFERSON0712@DIGITALCOLLEGE.LOCAL>
2024-07-23 10:35:28 -0300

    criando componente productPageComp e chamando na pagina ProductPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,43 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import Layout from "../compoments/Layout";
+import ProductPageComp from '../compoments/ProductPage/ProductPageComp'
+const ProductPage = () => {
+  return (
+    <>
+      <Layout>
+        <ProductPageComp />
+      </Layout>
+    </>
+  );
+}
 
-const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-};
-
-const ProductsPage = () => {
-    const query = useQuery();
-    const searchQuery = query.get('filter');
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        console.log('Search Query:', searchQuery);
-        const fetchProducts = async () => {
-            if (searchQuery) {
-                try {
-                    const response = await fetch(`http://localhost:3001/products?name_like=${searchQuery}`);
-                    const data = await response.json();
-                    setProducts(data);
-                } catch (error) {
-                    console.error('Error fetching products:', error);
-                }
-            }
-        };
-
-        fetchProducts();
-    }, [searchQuery]);
-
-    return (
-        <div>
-            <h1>Produtos</h1>
-            <p>Resultados da pesquisa: {searchQuery}</p>
-            <ul>
-                {products.map(product => (
-                    <li key={product.id}>{product.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-export default ProductsPage;
+export default ProductPage;
